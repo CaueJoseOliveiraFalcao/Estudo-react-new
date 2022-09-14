0489d85683658cfd5cbdd4b7d3254793a8f62453
Cauê Josê Oliveira Falcão <93996605+CaueJoseOliveiraFalcao@users.noreply.github.com>
2022-09-14 19:12:39 -0300

    FF

---
+++ ex/src/App.js
@@ -0,0 +1,13 @@
+import Papaia from "./components/Papaia";
+
+function App() {
+    return ( <
+        div className = "App" >
+        <
+        Papaia / >
+        <
+        /div>
+    );
+}
+
+export default App;
---
+++ ex/src/components/Papaia.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export default function Papaia(){
+    return(
+        <h1>olny papaia </h1>
+    )
+}
+
