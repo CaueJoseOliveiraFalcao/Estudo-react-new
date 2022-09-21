8c662f01bc45fe8f22d6cf844fab24b0050eb3b6
Cauê Josê Oliveira Falcão <93996605+CaueJoseOliveiraFalcao@users.noreply.github.com>
2022-09-21 12:25:18 -0300

    gg

--- ex/src/App.jsx
+++ ex/src/App.jsx
@@ -3,15 +3,34 @@ import Statelif from "./components/Statelif";
 import "./style.css";
 import Stasau from "./components/Stasau";
 import Papaia from "./components/Papaia"
+import Contato from "./pages/Contato";
+import Empresa from "./pages/Empresa";
+import Home from "./pages/Home";
+import {BrowserRouter as Router, Switch , Route , Link} from 'react-router-dom'
 
 function App() {
     const array = ['React' , 'Vue' , 'Angular']
     const [nome, setNome] = useState()
     return ( 
     <div className = "App" >
-        <Statelif setNome={setNome} />
-        <Stasau nome={nome}/>
-        <Papaia array={array}/>
+        <Router>
+            <ul>
+                <li><Link to='/'>Home</Link></li>
+                <li><Link to='/empresa'>Empresa</Link></li>
+                <li><Link to='/contato'>Contato</Link></li>
+                <Switch>
+                    <Route exact path="/">
+                        <Home/>
+                    </Route>
+                    <Route path="/empresa">
+                        <Empresa/>
+                    </Route>
+                    <Route path="/contato">
+                        <Contato/>
+                    </Route>
+                </Switch>
+            </ul>
+        </Router>
     </div>
     );
 }
