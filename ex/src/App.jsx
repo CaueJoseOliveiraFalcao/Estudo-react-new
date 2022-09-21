4c3fdd03662b16c985ff4d78fc814bc38f670a64
Cauê Josê Oliveira Falcão <93996605+CaueJoseOliveiraFalcao@users.noreply.github.com>
2022-09-21 11:57:47 -0300

    deixe estar

--- ex/src/App.jsx
+++ ex/src/App.jsx
@@ -2,14 +2,16 @@ import { useState } from "react";
 import Statelif from "./components/Statelif";
 import "./style.css";
 import Stasau from "./components/Stasau";
+import Papaia from "./components/Papaia"
 
 function App() {
-    //const array = ['React' , 'Vue' , 'Angular']
+    const array = ['React' , 'Vue' , 'Angular']
     const [nome, setNome] = useState()
     return ( 
     <div className = "App" >
         <Statelif setNome={setNome} />
         <Stasau nome={nome}/>
+        <Papaia array={array}/>
     </div>
     );
 }
