38533febe7fe2296dbad73c3b301d7815f5c3ad0
Cauê Josê Oliveira Falcão <93996605+CaueJoseOliveiraFalcao@users.noreply.github.com>
2022-09-22 11:41:21 -0300

    sas

--- ex/src/App.jsx
+++ ex/src/App.jsx
@@ -1,16 +1,17 @@
-import { useState } from "react";
-import Statelif from "./components/Statelif";
+//import { useState } from "react";
+//import Statelif from "./components/Statelif";
 import "./style.css";
-import Stasau from "./components/Stasau";
-import Papaia from "./components/Papaia"
+//import Stasau from "./components/Stasau";
+//import Papaia from "./components/Papaia"
 import Contato from "./pages/Contato";
 import Empresa from "./pages/Empresa";
 import Home from "./pages/Home";
-import {BrowserRouter as Router, Switch , Route , Link} from 'react-router-dom'
+import {BrowserRouter as Router, Routes , Route , Link} from 'react-router-dom'
+
 
 function App() {
-    const array = ['React' , 'Vue' , 'Angular']
-    const [nome, setNome] = useState()
+    //const array = ['React' , 'Vue' , 'Angular']
+    //const [nome, setNome] = useState()
     return ( 
     <div className = "App" >
         <Router>
@@ -18,17 +19,11 @@ function App() {
                 <li><Link to='/'>Home</Link></li>
                 <li><Link to='/empresa'>Empresa</Link></li>
                 <li><Link to='/contato'>Contato</Link></li>
-                <Switch>
-                    <Route exact path="/">
-                        <Home/>
-                    </Route>
-                    <Route path="/empresa">
-                        <Empresa/>
-                    </Route>
-                    <Route path="/contato">
-                        <Contato/>
-                    </Route>
-                </Switch>
+                <Routes>
+                    <Route exact path="/" element={<Home/>}/>
+                    <Route path='/empresa' element={<Empresa/>} />
+                    <Route path="/contato" element={<Contato/>}/>
+                </Routes>
             </ul>
         </Router>
     </div>
